ad0b792600a5f4e9060f0f4c7699405a28339a63
frhn16 <faroomirz@hotmail.com>
2023-11-12 15:51:51 +0000

    Add project files.

---
+++ ConnectFour/Pieces.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConnectFour
+{
+    class ConnectFourPiece
+    {
+        public ConnectFourPiece(int xCoordinate, int yCoordinate)
+        {
+            this.PieceType = " ";
+            this.XCoordinate = xCoordinate;
+            this.YCoordinate = yCoordinate;
+        }
+
+        public ConnectFourPiece(String pieceType, int xCoordinate, int yCoordinate)
+        {
+            this.PieceType = pieceType;
+            this.XCoordinate = xCoordinate;
+            this.YCoordinate = yCoordinate;
+        }
+
+        public String PieceType
+        {
+            get; set;
+        }
+
+        public int XCoordinate
+        {
+            get; set;
+        }
+
+        public int YCoordinate
+        {
+            get; set;
+        }
+    }
+}
---
+++ ConnectFour/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConnectFour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectFourPiece[,] gameBoard = new ConnectFourPiece[6, 7];
+            for (int x = 0; x < gameBoard.GetLength(0); x++)
+            {
+                for (int y = 0; y < gameBoard.GetLength(1); y++)
+                {
+                    gameBoard[x, y] = new ConnectFourPiece(x, y);
+                }
+            }
+            for (int x = 0; x < gameBoard.GetLength(0); x++)
+            {
+                for (int y = 0; y < gameBoard.GetLength(1); y++)
+                {
+                    Console.Write(gameBoard[x, y].PieceType + "|");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
