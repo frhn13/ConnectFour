1ee4fcf77c3ce0a4088fca5b76654514997ce540
frhn16 <faroomirz@hotmail.com>
2023-11-14 21:05:09 +0000

    Game can now be won horizontally or vertically

--- ConnectFour/Program.cs
+++ ConnectFour/Program.cs
@@ -9,6 +9,7 @@ namespace ConnectFour
             bool gameWon = false;
             String currentPlayer = "R";
             int columnPlaced;
+            String winner = " ";
             
             ConnectFourPiece[,] gameBoard = new ConnectFourPiece[6, 7];
             for (int x = 0; x < gameBoard.GetLength(0); x++)
@@ -35,13 +36,16 @@ namespace ConnectFour
                 Console.WriteLine("Which column do you want to place the piece in?");
                 try
                 {
-                    columnPlaced = Convert.ToInt32(Console.ReadLine());
+                    columnPlaced = Convert.ToInt32(Console.ReadLine()) - 1;
                     for (int x = gameBoard.GetLength(0) - 1; x >= 0; x--)
                     {
                         if (gameBoard[x, columnPlaced].PieceType == " ")
                         {
                             gameBoard[x, columnPlaced].PieceType = currentPlayer;
                             currentPlayer = (currentPlayer == "R") ? "B" : "R";
+                            winner = FindWinner(gameBoard, gameBoard[x, columnPlaced]);
+                            if (winner != " ")
+                                gameWon = true;
                             break;
                         }
                         if (x == 0)
@@ -56,9 +60,70 @@ namespace ConnectFour
                 {
                     Console.WriteLine("The column number entered is too big or too small, try again.");
                 }
-                
+            }
+            if (winner == "R")
+                Console.WriteLine("Red has won!");
+            else if (winner == "B")
+                Console.WriteLine("Blue has won!");
+            else
+                Console.WriteLine("No one has won!");
+            for (int x = 0; x < gameBoard.GetLength(0); x++)
+            {
+                for (int y = 0; y < gameBoard.GetLength(1); y++)
+                {
+                    Console.Write(gameBoard[x, y].PieceType + "|");
+                }
+                Console.WriteLine();
             }
             Console.ReadKey();
+        }
+        static String FindWinner(ConnectFourPiece[,] gameBoard, ConnectFourPiece piecePlaced)
+        {
+            Console.WriteLine($"{piecePlaced.PieceType} {piecePlaced.XCoordinate} {piecePlaced.YCoordinate}");
+            if (piecePlaced.XCoordinate <= 3 &&
+                piecePlaced.PieceType == gameBoard[piecePlaced.XCoordinate + 1, piecePlaced.YCoordinate].PieceType &&
+                piecePlaced.PieceType == gameBoard[piecePlaced.XCoordinate + 2, piecePlaced.YCoordinate].PieceType &&
+                piecePlaced.PieceType == gameBoard[piecePlaced.XCoordinate + 3, piecePlaced.YCoordinate].PieceType)
+            {
+                return piecePlaced.PieceType;
+            }
+            if (piecePlaced.YCoordinate <= 4)
+            {
+                if (piecePlaced.PieceType == gameBoard[piecePlaced.XCoordinate, piecePlaced.YCoordinate + 1].PieceType &&
+                piecePlaced.PieceType == gameBoard[piecePlaced.XCoordinate, piecePlaced.YCoordinate + 2].PieceType &&
+                piecePlaced.PieceType == gameBoard[piecePlaced.XCoordinate, piecePlaced.YCoordinate + 3].PieceType)
+                {
+                    return piecePlaced.PieceType;
+                }
+            }
+            if (piecePlaced.YCoordinate <= 5 && piecePlaced.YCoordinate >= 1)
+            {
+                if (piecePlaced.PieceType == gameBoard[piecePlaced.XCoordinate, piecePlaced.YCoordinate - 1].PieceType &&
+                piecePlaced.PieceType == gameBoard[piecePlaced.XCoordinate, piecePlaced.YCoordinate + 1].PieceType &&
+                piecePlaced.PieceType == gameBoard[piecePlaced.XCoordinate, piecePlaced.YCoordinate + 2].PieceType)
+                {
+                    return piecePlaced.PieceType;
+                }
+            }
+            if (piecePlaced.YCoordinate <= 6 && piecePlaced.YCoordinate >= 3)
+            {
+                if (piecePlaced.PieceType == gameBoard[piecePlaced.XCoordinate, piecePlaced.YCoordinate - 2].PieceType &&
+                piecePlaced.PieceType == gameBoard[piecePlaced.XCoordinate, piecePlaced.YCoordinate - 1].PieceType &&
+                piecePlaced.PieceType == gameBoard[piecePlaced.XCoordinate, piecePlaced.YCoordinate + 1].PieceType)
+                {
+                    return piecePlaced.PieceType;
+                }
+            }
+            if (piecePlaced.YCoordinate <= 7)
+            {
+                if (piecePlaced.PieceType == gameBoard[piecePlaced.XCoordinate, piecePlaced.YCoordinate - 3].PieceType &&
+                piecePlaced.PieceType == gameBoard[piecePlaced.XCoordinate, piecePlaced.YCoordinate - 2].PieceType &&
+                piecePlaced.PieceType == gameBoard[piecePlaced.XCoordinate, piecePlaced.YCoordinate - 1].PieceType)
+                {
+                    return piecePlaced.PieceType;
+                }
+            }
+            return " ";
         }
     }
 }
