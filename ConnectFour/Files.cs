373be4ad36267985095c0c3643f14f77c4d47e74
frhn16 <faroomirz@hotmail.com>
2023-11-16 22:22:56 +0000

    Now allow the user to restart the game after finishing and began implementing file handling

---
+++ ConnectFour/Files.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace ConnectFour
+{
+    class Files
+    {
+        private static readonly string rootPath = AppDomain.CurrentDomain.BaseDirectory;
+        private static String fileName = "Winners.txt";
+
+        public static void WriteToFile(String name)
+        {
+            using (StreamWriter outputFile = new StreamWriter(Path.Combine(rootPath, fileName)))
+            {
+                outputFile.WriteLine(name);
+            }
+        }
+
+        public static List<String> ReadFromFile()
+        {
+            List<String> winners = new List<String>();
+            String winner;
+            StreamReader readFile = null;
+            try
+            {
+                readFile = new StreamReader(Path.Combine(rootPath, fileName));
+                do
+                {
+                    winner = readFile.ReadLine();
+                    winners.Add(winner);
+                } while (winner != null);
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine("No winners yet.");
+            }
+            finally
+            {
+                readFile.Close();
+            }
+            return winners;
+        }
+    }
+}
--- ConnectFour/Program.cs
+++ ConnectFour/Program.cs
@@ -6,21 +6,82 @@ namespace ConnectFour
     {
         static void Main(string[] args)
         {
-            bool gameWon = false;
-            String currentPlayer = "R";
+            bool gameWon;
+            String currentPlayer;
             int columnPlaced;
-            String winner = " ";
-            
-            ConnectFourPiece[,] gameBoard = new ConnectFourPiece[6, 7];
-            for (int x = 0; x < gameBoard.GetLength(0); x++)
+            String winner;
+            String playAgainStr;
+            bool playAgain = true;
+
+            do
             {
-                for (int y = 0; y < gameBoard.GetLength(1); y++)
+                gameWon = false;
+                winner = " ";
+                currentPlayer = "R";
+
+                ConnectFourPiece[,] gameBoard = new ConnectFourPiece[6, 7];
+                for (int x = 0; x < gameBoard.GetLength(0); x++)
                 {
-                    gameBoard[x, y] = new ConnectFourPiece(x, y);
+                    for (int y = 0; y < gameBoard.GetLength(1); y++)
+                    {
+                        gameBoard[x, y] = new ConnectFourPiece(x, y);
+                    }
                 }
-            }
-            while (!gameWon)
-            {
+                while (!gameWon)
+                {
+                    Console.WriteLine();
+                    for (int x = 0; x < gameBoard.GetLength(0); x++)
+                    {
+                        for (int y = 0; y < gameBoard.GetLength(1); y++)
+                        {
+                            if (y < gameBoard.GetLength(1) - 1)
+                                Console.Write(gameBoard[x, y].PieceType + "|");
+                            else
+                                Console.Write(gameBoard[x, y].PieceType);
+                        }
+                        if (x < gameBoard.GetLength(0) - 1)
+                            Console.WriteLine("\n-------------");
+                    }
+                    Console.WriteLine("\n1 2 3 4 5 6 7\n");
+                    if (currentPlayer == "R")
+                        Console.WriteLine("Red's turn");
+                    else
+                        Console.WriteLine("Blue's turn");
+                    Console.WriteLine("Which column do you want to place the piece in?");
+                    try
+                    {
+                        columnPlaced = Convert.ToInt32(Console.ReadLine()) - 1;
+                        for (int x = gameBoard.GetLength(0) - 1; x >= 0; x--)
+                        {
+                            if (gameBoard[x, columnPlaced].PieceType == " ")
+                            {
+                                gameBoard[x, columnPlaced].PieceType = currentPlayer;
+                                currentPlayer = (currentPlayer == "R") ? "B" : "R";
+                                winner = FindWinner(gameBoard, gameBoard[x, columnPlaced]);
+                                if (winner != " ")
+                                    gameWon = true;
+                                break;
+                            }
+                            if (x == 0)
+                                Console.WriteLine("No more spaces in that column, try again.");
+                        }
+                    }
+                    catch (FormatException e)
+                    {
+                        Console.WriteLine("Please enter an integer as the column number.");
+                    }
+                    catch (IndexOutOfRangeException e)
+                    {
+                        Console.WriteLine("The column number entered is too big or too small, try again.");
+                    }
+
+                }
+                if (winner == "R")
+                    Console.WriteLine("Red has won!");
+                else if (winner == "B")
+                    Console.WriteLine("Blue has won!");
+                else
+                    Console.WriteLine("No one has won!");
                 for (int x = 0; x < gameBoard.GetLength(0); x++)
                 {
                     for (int y = 0; y < gameBoard.GetLength(1); y++)
@@ -29,53 +90,13 @@ namespace ConnectFour
                     }
                     Console.WriteLine();
                 }
-                if (currentPlayer == "R")
-                    Console.WriteLine("Red's turn");
-                else
-                    Console.WriteLine("Blue's turn");
-                Console.WriteLine("Which column do you want to place the piece in?");
-                try
-                {
-                    columnPlaced = Convert.ToInt32(Console.ReadLine()) - 1;
-                    for (int x = gameBoard.GetLength(0) - 1; x >= 0; x--)
-                    {
-                        if (gameBoard[x, columnPlaced].PieceType == " ")
-                        {
-                            gameBoard[x, columnPlaced].PieceType = currentPlayer;
-                            currentPlayer = (currentPlayer == "R") ? "B" : "R";
-                            winner = FindWinner(gameBoard, gameBoard[x, columnPlaced]);
-                            if (winner != " ")
-                                gameWon = true;
-                            break;
-                        }
-                        if (x == 0)
-                            Console.WriteLine("No more spaces in that column, try again.");
-                    }
-                }
-                catch (FormatException e)
-                {
-                    Console.WriteLine("Please enter an integer as the column number.");
-                }
-                catch (IndexOutOfRangeException e)
-                {
-                    Console.WriteLine("The column number entered is too big or too small, try again.");
-                }
+                Console.WriteLine("Would you like to play again?");
+                playAgainStr = Console.ReadLine();
+                playAgain = playAgainStr.ToLower() == "yes" || playAgainStr.ToLower() == "y";
 
-            }
-            if (winner == "R")
-                Console.WriteLine("Red has won!");
-            else if (winner == "B")
-                Console.WriteLine("Blue has won!");
-            else
-                Console.WriteLine("No one has won!");
-            for (int x = 0; x < gameBoard.GetLength(0); x++)
-            {
-                for (int y = 0; y < gameBoard.GetLength(1); y++)
-                {
-                    Console.Write(gameBoard[x, y].PieceType + "|");
-                }
-                Console.WriteLine();
-            }
+                //Files.WriteToFile();
+            } while (playAgain);
+            
             Console.ReadKey();
         }
         static String FindWinner(ConnectFourPiece[,] gameBoard, ConnectFourPiece piecePlaced)
